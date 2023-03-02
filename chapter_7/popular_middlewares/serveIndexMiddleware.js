e00d9b39a33987b93249ff041977e964062acb4b
Prajjwal Rodger Massey <mrp@ghost.mrp>
2023-03-02 06:09:31 +0530

    Added new programs from chapter 7

---
+++ chapter_7/popular_middlewares/serveIndexMiddleware.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const serveIndex = require('serve-index')
+const serveStatic = require('serve-static')
+
+/* In case that the serve-static middleware is unable to resolve path it will then move onto the serve-index middleware
+    which will show use the entire directory present at the specified path and we could search on the files present in that directory */
+express().use(serveStatic(__dirname+'/public')).use(serveIndex(__dirname+'/public')).listen(8080)
---
+++ chapter_7/popular_middlewares/serveStaticMiddleware.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const serveStatic = require('serve-static')
+
+const app = express();
+
+app.use(serveStatic(__dirname+'/public')).listen(8080)
+
+/* We can also specify a list of files to be treated as index files */
+/* serveStatic(__dirname+'/public', {'index':['default.html','default.htm']}) */
+/* We can also use express framework directly to call servestatic as it is also a part of express ecosystem*/
+/* express.static(__dirname+'/public') */
